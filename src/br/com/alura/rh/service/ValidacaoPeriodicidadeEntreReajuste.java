ad598a9f58c9517172f1bf84a8261a76a8471606
Saulo de Carvalho <saulocn@gmail.com>
2021-05-28 20:41:10 -0300

    Reduzindo o acoplamento e separando as regras de validações

---
+++ src/br/com/alura/rh/service/ValidacaoPeriodicidadeEntreReajuste.java
@@ -0,0 +1,21 @@
+package br.com.alura.rh.service;
+
+import br.com.alura.rh.ValidacaoException;
+import br.com.alura.rh.model.Funcionario;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class ValidacaoPeriodicidadeEntreReajuste implements ValidacaoReajuste {
+
+    public void validar(Funcionario funcionario, BigDecimal aumento) {
+        LocalDate dataUltimoReajuste = funcionario.getDataUltimoReajuste();
+        LocalDate hoje = LocalDate.now();
+        long mesesDoUltimoReajuste = ChronoUnit.MONTHS.between(dataUltimoReajuste, hoje);
+
+        if (mesesDoUltimoReajuste < 6) {
+            throw new ValidacaoException("Funcionário só deve ter reajuste após 6 meses do último!");
+        }
+    }
+}
